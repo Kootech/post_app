fe1478638ed31f349b5817cb492f7434b691c621
kelvin <kitetesh.km@gmail.com>
2022-08-26 12:15:51 +0300

    modified post to work with mongodb

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,15 +1,56 @@
+import postModel from "../models/post.js";
+
 export const getPosts = async (req, res) => {
-  res.send(`get post is connected`);
+  try {
+    const post = await postModel.find();
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(`error occured ${error}`);
+  }
 };
 
 export const addPost = async (req, res) => {
-  res.send(`add post is connected`);
+  if (!req.body) {
+    res.status(400).send(`please add body`);
+  }
+  const { title, description } = req.body;
+  try {
+    const post = await postModel.create({
+      title,
+      description,
+    });
+    res.status(201).json(post);
+  } catch (error) {
+    console.log(`error occorred on adding a post`);
+  }
 };
 
 export const updatePost = async (req, res) => {
-  res.send(`update post is connected`);
+  const findpost = await postModel.findById(req.params.id);
+
+  if (!findpost) {
+    res.status(404).json({ message: "post not found" });
+  }
+  try {
+    const post = await postModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(`error occured on update`);
+  }
 };
 
 export const deletePost = async (req, res) => {
-  res.send(`delete is connected`);
+  const findpost = await postModel.findById(req.params.id);
+
+  if (!findpost) {
+    res.status(404).json({ message: "post not found" });
+  }
+  try {
+    const post = await postModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(`error occurred on delete`);
+  }
 };
